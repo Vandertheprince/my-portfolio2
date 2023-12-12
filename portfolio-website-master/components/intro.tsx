8baf11a8b5f6d9004771c1784c14401a9a489847
vandertheprince <109791828+Vandertheprince@users.noreply.github.com>
2023-12-13 04:33:35 +0800

    Update intro.tsx

--- portfolio-website-master/components/intro.tsx
+++ portfolio-website-master/components/intro.tsx
@@ -31,7 +31,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="../public/nelz.jpg"
+              src="./public/nelz.jpg"
               alt="nelson"
               width="192"
               height="192"
